646b96a412ed62006bbc4d0f12c0edd48a205b58
Kathan Sanghavi <skathan@outlook.com>
2022-08-24 22:30:21 +0530

    Generate Image with a Button Click

--- WebApp/app.py
+++ WebApp/app.py
@@ -1,7 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template, url_for, request
+import GeneratePicture
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template("index.html")
+
+@app.route('/result',methods=['POST', 'GET'])
+def result():
+    output = request.form.to_dict()
+    print(output)
+    name = output["name"]
+    GeneratePicture.generate_picture(name)
+    return render_template('photo.html')
